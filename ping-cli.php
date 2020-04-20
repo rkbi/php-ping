fd270358df1afb40b44dc5f2fbbb07076955cd7c
rkbssl <49746492+rkbssl@users.noreply.github.com>
2020-04-20 14:14:14 +0600

    Create ping-cli.php

---
+++ ping-cli.php
@@ -0,0 +1,27 @@
+<?php
+// ping start
+$ports = [80, 443];
+$hosts = [
+        'example.com',
+        '144.76.92.216',
+        'sandbox.sslcommerz.com',
+        '103.26.139.148',
+        'securepay.sslcommerz.com'
+    ];
+
+$waitTimeoutInSeconds = 1;
+for ($i = 0; $i < count($hosts); $i++) {
+    for ($j = 0; $j < count($ports); $j++) {
+        $host = $hosts[$i];
+        $port = $ports[$j];
+        if ($fp = @fsockopen($host, $port, $errCode, $errStr, $waitTimeoutInSeconds)) {
+            echo "connected to $host on port $port" . PHP_EOL;
+        } else {
+            echo "can not connect to $host on port $port" . PHP_EOL;
+        }
+    }
+    echo PHP_EOL;
+}
+@fclose($fp);
+exit;
+// ping end
